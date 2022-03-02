6802a8ff1b142939be27a624c912323b8db4f55b
unknown <mxngoc2104@gmail.com>
2022-03-02 22:39:46 +0700

    fixed SQLAlchemy errors

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if uri and uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'uri'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = '123'
 api = Api(app)
